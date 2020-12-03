54b89d13eb7787c0a316f0576ef42f77c4fea8fe
Per Unneberg <per.unneberg@scilifelab.se>
2020-12-03 12:56:51 +0100

    Update lab and add genetic map

--- session_recurrentNeuralNetworks/lab_recombination_rate/utils.py
+++ session_recurrentNeuralNetworks/lab_recombination_rate/utils.py
@@ -4,9 +4,14 @@ import os
 import random
 import numpy as np
 import subprocess
+import pickle
 import tskit
+import msprime
+import scipy.stats
+import copy
 import pandas as pd
 import tensorflow as tf
+from matplotlib import pyplot as plt
 from tensorflow.keras import layers
 from tensorflow.keras.models import Model
 
@@ -58,7 +63,8 @@ def pad(haps, pos, maxsites=None, frameWidth=0):
         fw = frameWidth
         haps = np.pad(haps,((fw,fw), (fw,fw)),"constant",constant_values=2.0)
         pos = np.pad(pos,((fw,fw)),"constant",constant_values=-1.0)
-
+    haps = np.where(haps > 1.0, -1, haps)
+    pos = np.where(pos == -1.0, 0, pos)
     return haps, pos
 
 def fill_pad(haps, pos, maxlen):
@@ -81,19 +87,30 @@ def fill_pad(haps, pos, maxlen):
 class sequenceBatchGenerator(tf.keras.utils.Sequence):
     """Generator class to provide data in batches"""
 
-    def __init__(self, datadir, maxlen, batchSize=16, frameWidth=0, seed=None):
+    def __init__(self, datadir, maxlen, batchSize=64, frameWidth=0, seed=None):
         self.datadir = datadir
-        self.params = pd.read_table(os.path.join(datadir, "params.txt"))
-        self.n_data = self.params.shape[0]
+        self.info = pickle.load(open(os.path.join(datadir, "info.p"), "rb"))
+        self.n_data = self.info["nreps"]
         self.maxlen = maxlen
         self.batchSize = batchSize
         self.frameWidth = frameWidth
+        self.rhoZ = self.normalizeRho()
         self.on_epoch_end()
 
     def on_epoch_end(self):
         """Shuffle indices to randomize order of training examples on epoch end"""
         self.indices = np.arange(self.n_data)
         np.random.shuffle(self.indices)
+
+    def normalizeRho(self):
+        """Convert rho to Z-scores"""
+        rhoZ = copy.deepcopy(self.info["rho"])
+        rho_mean = np.mean(rhoZ, axis=0)
+        rho_sd = np.std(rhoZ, axis=0)
+        rhoZ -= rho_mean
+        rhoZ = np.divide(rhoZ, rho_sd, out=np.zeros_like(rhoZ),
+                         where=rho_sd != 0)
+        return rhoZ
 
     def __len__(self):
         """Get the number of batches per epoch as the number of data points divided by batch size"""
@@ -107,22 +124,22 @@ class sequenceBatchGenerator(tf.keras.utils.Sequence):
 
     def __data_generation(self, batchIndices):
         """Required core function to generate data batch"""
-        rho = np.reshape(self.params["rho"].to_numpy(), (-1, 1))
-        rhoZ = (rho - np.mean(rho, axis=0)) / np.std(rho, axis=0)
         haps = []
         pos = []
         # Read one dataset at a time
         for i in batchIndices:
             H = np.load(os.path.join(self.datadir, f"{i}_haps.npy"))
             P = np.load(os.path.join(self.datadir, f"{i}_pos.npy"))
-            # Any shuffling of individuals, reorganization of
+            P = P / self.info["length"]
+            # Add shuffling of individuals, reorganization of
             # haplotypes takes place here
             hpad, ppad = pad(H, P, self.maxlen, frameWidth=self.frameWidth)
             haps.append(hpad)
             pos.append(ppad)
         haps = np.array(haps, dtype="float32") # (batchSize, nSites, nsamples)
         pos = np.array(pos, dtype="float32") # (batchSize, nSites)
-        return [haps, pos], np.array(rhoZ[batchIndices]) # X, y
+        rhoZ = [[t] for t in self.rhoZ[batchIndices]]
+        return [haps, pos], np.array(rhoZ) # X, y
 
 
 def simulate(nreps, outdir, seed=42, murange=(0, 2e-7), rhorange=(0, 2e-7), sample_size=10, Ne=1e4, length=1e4, **kwargs):
@@ -142,18 +159,20 @@ def simulate(nreps, outdir, seed=42, murange=(0, 2e-7), rhorange=(0, 2e-7), samp
         os.makedirs(outdir)
     mu = [np.random.uniform(murange[0], murange[1]) for _ in range(nreps)]
     rho = [np.random.uniform(rhorange[0], rhorange[1]) for _ in range(nreps)]
-    segregating_sites = np.empty(nreps, dtype="int64")
-    fh = open(os.path.join(outdir, "params.txt"), "w")
-    fh.write("ID\tmu\trho\tS\tpi\n")
+    S = np.empty(nreps, dtype="int64")
     for i in range(nreps):
-        ts = msprime.simulate(random_seed=seed, sample_size=sample_size, Ne=Ne, length=length,
-                              mutation_rate=mu[i], recombination_rate=rho[i], **kwargs)
+        ts = msprime.simulate(random_seed=seed,
+                              sample_size=sample_size, Ne=Ne,
+                              length=length, mutation_rate=mu[i],
+                              recombination_rate=rho[i], **kwargs)
         np.save(os.path.join(outdir, f"{i}_haps.npy"), ts.genotype_matrix())
-        np.save(os.path.join(outdir, f"{i}_pos.npy"), np.array([s.position for s in ts.sites()], dtype="float32"))
-        segregating_sites[i] = ts.get_num_sites()
-        fh.write(f"{i}\t{mu[i]}\t{rho[i]}\t{ts.get_num_sites()}\t{ts.diversity()}\n")
-    fh.close()
-
+        np.save(os.path.join(outdir, f"{i}_pos.npy"),
+                np.array([s.position for s in ts.sites()], dtype="float32"))
+        S[i] = ts.get_num_sites()
+    info = {'mu': mu, 'rho': rho, 'S': S, 'nreps': nreps, 'dataset': outdir,
+            'sample_size': sample_size, 'Ne': Ne, 'length': length}
+    with open(os.path.join(outdir, "info.p"), "wb") as fh:
+        pickle.dump(info, fh)
 
 def GRU(x, y):
     """Gated recurrent unit model.
@@ -171,18 +190,18 @@ def GRU(x, y):
     # Define input layer that takes a genotype matrix; recall that genotype matrix stores data as (nsites, nsamples)
     genotype_inputs = layers.Input(shape=(nsites, nsamples))
     # analyze sequences in both directions, GRU more computationally efficient than LSTM
-    model = layers.Bidirectional(layers.GRU(21, return_sequences=False))(genotype_inputs)
+    model = layers.Bidirectional(layers.GRU(84, return_sequences=False))(genotype_inputs)
     # Add dense layer with 256 neurons in output space
-    model = layers.Dense(64)(model)
+    model = layers.Dense(256)(model)
 
     # Define input layer for positions
     position_inputs = layers.Input(shape=(npos,))
-    m2 = layers.Dense(64)(position_inputs)
+    m2 = layers.Dense(256)(position_inputs)
 
     # Concatenate genotype matrix and position input layers
     model = layers.concatenate([model, m2])
     # Add Dense and output layer that outputs a (normalized) recombination value
-    model = layers.Dense(16)(model)
+    model = layers.Dense(64)(model)
     output = layers.Dense(1)(model)
 
     model = Model(inputs=[genotype_inputs, position_inputs], outputs=[output])
@@ -208,20 +227,20 @@ def GRU_DROPOUT(x, y):
     # Define input layer that takes a genotype matrix; recall that genotype matrix stores data as (nsites, nsamples)
     genotype_inputs = layers.Input(shape=(nsites, nsamples))
     # analyze sequences in both directions, GRU more computationally efficient than LSTM
-    model = layers.Bidirectional(layers.GRU(21, return_sequences=False))(genotype_inputs)
+    model = layers.Bidirectional(layers.GRU(84, return_sequences=False))(genotype_inputs)
     # Add Dense layer with 256 neurons in output space
-    model = layers.Dense(64)(model)
+    model = layers.Dense(256)(model)
     # Add Dropout layer to randomly set input units to 0 with rate 0.35 to prevent overfitting
     model = layers.Dropout(0.35)(model)
 
     # Define input layer for positions
     position_inputs = layers.Input(shape=(npos,))
-    m2 = layers.Dense(64)(position_inputs)
+    m2 = layers.Dense(256)(position_inputs)
 
     # Concatenate genotype matrix and position input layers
     model = layers.concatenate([model, m2])
     # Add Dense and output layer that outputs a (normalized) recombination value
-    model = layers.Dense(16)(model)
+    model = layers.Dense(64)(model)
     # Add Dropout layer to randomly set input units to 0 with rate 0.35 to prevent overfitting
     model = layers.Dropout(0.35)(model)
     output = layers.Dense(1)(model)
@@ -231,3 +250,34 @@ def GRU_DROPOUT(x, y):
     model.summary()
 
     return model
+
+
+def _get_recombination_map_data(recomb_map):
+    """Get positions and rates for a recombination map for plotting"""
+    positions = np.array(recomb_map.get_positions())
+    rates = np.array(recomb_map.get_rates())
+    num_bins = 500
+    v, bin_edges, _ = scipy.stats.binned_statistic(
+        positions, rates, bins=num_bins)
+    x = bin_edges[:-1][np.logical_not(np.isnan(v))]
+    y = v[np.logical_not(np.isnan(v))]
+    return x, y
+
+def plot_recombination_map(recomb_map):
+    x, y = _get_recombination_map_data(recomb_map)
+    fig, ax = plt.subplots(figsize=(16, 6))
+    ax.plot(x, y, color="blue")
+    ax.set_ylabel("Recombination rate")
+    ax.set_xlabel("Chromosome position")
+    return fig, ax
+
+##############################
+# FIXME: implement functions below for lab
+##############################
+class gtBatchGenerator(tf.keras.utils.Sequence):
+    """Generator to split simulated tree sequence in windows, padded and all"""
+    pass
+
+def predict_windows(model, generator):
+    """Use model to predict genotype matrix windows generated by gtBatchGenerator"""
+    pass
