9a1497b9f446de6007fdad1f32e975d368776c75
clami66 <claudio.mirabello@scilifelab.se>
2021-06-07 14:27:10 +0200

    added necessary to convert slides to html

---
+++ common_assets/ipynb_2_html.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+notebooks=$(find ../ -name "*.ipynb" -not -path '*/.*')
+
+for notebook in $notebooks; do
+    isslide=$(grep "\"slide_type\": \"slide\"" $notebook)
+    
+    if [[ ! -z $isslide ]]; then
+        if [[ $notebook == *"session_annBuildingBlocks_"* ]]; then
+            jupyter nbconvert --to slides $notebook --no-input --template slides_reveal_split.tpl
+        else
+            jupyter nbconvert --to slides $notebook --template slides_reveal_split.tpl
+        fi
+    else
+        jupyter nbconvert --to html $notebook
+    fi
+done
+
+slides=$(find ../ -name "*.slides.html" -not -path '*/.*')
+scriptspath=$(pwd)
+for slide in $slides; do
+    echo "Converting $slide to self-contained"
+    todir=$(dirname $slide)
+    slidefile=$(basename $slide)
+    cd $todir
+    python $scriptspath/convert_html_to_standalone.py --infile $slidefile --outfile $slidefile
+    cd -
+done
